3523f25ffe94dfb57110f5ae82f4c4cbeda3902d
xiwanghuolight <145749419+xiwanghuolight@users.noreply.github.com>
2025-07-15 21:53:23 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .o3d import npy2pcd, pcd2npy, get_correspondences, execute_global_registration, npy2feat, normal
-from .yaml import decode_config
-from .process import gather_points, square_dists, setup_seed, sample_and_group, angle, \
-    fmat, to_tensor
